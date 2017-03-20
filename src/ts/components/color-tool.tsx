44d4728ca910beec7871f6d60bce01fd3599f7f0
Eric W. Greene <eric@ericwgreene.com>
2017-03-20 15:24:08 -0400

    added code demos

--- src/ts/components/color-tool.tsx
+++ src/ts/components/color-tool.tsx
@@ -4,17 +4,40 @@ interface ColorToolProps {
     colors: string[];
 }
 
-export class ColorTool extends React.Component<ColorToolProps, void> {
+interface ColorToolState {
+    newColor: string;
+}
+
+export class ColorTool extends React.Component<ColorToolProps, ColorToolState> {
+
+    constructor(props: ColorToolProps) {
+        super(props);
+
+        this.state = {
+            newColor: "green",
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    public onChange(e: any) {
+        this.setState({
+            newColor: e.target.value,
+        });
+    }
 
     public render() {
-
-        console.log(this.props.colors);
 
         return <div>
             <h1>Color Tool</h1>
             <ul>
                 {this.props.colors.map((color) => <li>{color}</li>)}
             </ul>
+            <form>
+                <label htmlFor="new-color-input">New Color:</label>
+                <input type="text" id="new-color-input"
+                    value={this.state.newColor} onChange={this.onChange} />
+            </form>
         </div>;
 
     }
