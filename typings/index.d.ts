bb85af2e828be63e82cc30387806dc932c98d0a4
Eric W. Greene <eric@ericwgreene.com>
2017-03-20 07:59:28 -0400

    fix to initial commit

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="modules/react-dom/index.d.ts" />
-/// <reference path="modules/react-router/index.d.ts" />
-/// <reference path="modules/react/index.d.ts" />
